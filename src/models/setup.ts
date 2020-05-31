ccbbc1b26565bdb24e4e9064b52466afacbe2ea6
Daniil Kozlov <d.kzlv.ru@gmail.com>
2020-05-31 15:20:28 +0300

    Adding cls-hooked for automatic transactions

--- src/models/setup.ts
+++ src/models/setup.ts
@@ -1,6 +1,11 @@
 import { Sequelize } from 'sequelize-typescript'
 import { join } from 'path'
+import { createNamespace } from 'cls-hooked'
+
 import config from './config/index.js'
+
+const namespace = createNamespace('dbTransaction')
+Sequelize.useCLS(namespace)
 
 const sequelize = new Sequelize(config[process.env.NODE_ENV as string])
 sequelize.addModels([join(__dirname, '**', '*.model.ts')])
