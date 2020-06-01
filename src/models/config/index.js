35f9fb588503f8d36d426c732c77d124777899f1
Daniil Kozlov <d.kzlv.ru@gmail.com>
2020-06-01 15:54:30 +0300

    Now tests use PostgreSQL

--- src/models/config/index.js
+++ src/models/config/index.js
@@ -1,28 +1,36 @@
-module.exports = {
-  development: {
-    database: 'app',
-    dialect: 'sqlite',
-    storage: 'db.sqlite',
+// For migration and other CLI commands
+if (!process.env.DB_HOST) require('dotenv').config()
+
+const allCommonConfig = {
+    dialect: 'postgres',
     logging: false,
   },
-  test: {
-    database: 'app',
-    dialect: 'sqlite',
-    storage: 'db.test.sqlite',
-    logging: false,
-  },
-  production: {
+  commonConfig = {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    dialect: 'postgres',
+    port: process.env.DB_PORT,
+    ...allCommonConfig,
+  }
+
+module.exports = {
+  development: commonConfig,
+  production: {
+    ...commonConfig,
     pool: {
       max: 5,
       min: 0,
       acquire: 30000,
       idle: 10000,
     },
-    logging: false,
+  },
+  test: {
+    username: process.env.DB_USERNAME_TEST,
+    password: process.env.DB_PASSWORD_TEST,
+    database: process.env.DB_NAME_TEST,
+    host: process.env.DB_HOST_TEST,
+    port: process.env.DB_PORT_TEST,
+    ...allCommonConfig,
   },
 }
