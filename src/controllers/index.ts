ddb664ec9194540aec45ce8e44bf263c68a42bec
Daniil Kozlov <d.kzlv.ru@gmail.com>
2020-08-23 17:09:23 +0300

    Adding data router to api router

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -3,6 +3,7 @@ import { Router } from 'express'
 import { errorHandler } from '@/middlewares/errorHandler'
 
 import { authRouter } from '@/controllers/auth'
+import { dataRouter } from '@/controllers/data'
 import { walletRouter } from '@/controllers/wallet'
 import { directoryRouter } from '@/controllers/directory'
 
@@ -11,6 +12,7 @@ const apiRouter = Router()
 apiRouter
   .use('/auth', authRouter)
   .use('/wallet', walletRouter)
+  .use('/data', dataRouter)
   .use('/directory', directoryRouter)
   .use((_, res) => res.status(404).json({ error: 'No such path' }).end())
   .use(errorHandler)
