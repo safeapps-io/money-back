34fe119dd1e48af389bef5c664137711835815b8
Daniil Kozlov <d.kzlv.ru@gmail.com>
2021-04-20 12:08:18 +0300

    Updating check-script

--- src/_check.ts
+++ src/_check.ts
@@ -2,10 +2,11 @@ import dotenv from 'dotenv'
 import _ from 'lodash'
 dotenv.config()
 
-import sequelize from './models/setup'
+import sequelize from '@/models/setup'
+import { initRedisConnection } from '@/services/redis/connection'
 
 async function main() {
-  await sequelize.sync()
+  await Promise.all([sequelize.sync(), initRedisConnection()])
 
   // Do stuff here, log it, launch using `yarn run:check`
   console.log(JSON.stringify({}, null, 2))
