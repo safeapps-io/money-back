6546f195bde07bc65c1fd70046fbaba749a5215f
Daniil Kozlov <d.kzlv.ru@gmail.com>
2021-04-28 15:35:26 +0300

    Sorting currencies based on IP address

--- src/services/directory/directoryService.ts
+++ src/services/directory/directoryService.ts
@@ -1,12 +1,30 @@
+import { lookup } from 'geoip-country'
+import arrayMove from 'array-move'
+import _ from 'lodash'
+
 import { MetaCategoryManager } from '@/models/metaCategory.model'
 import { SchemeManager } from '@/models/scheme.model'
 import { serializeModel, Serializers } from '@/models/serializers'
-import currencyData from './dataSources/currencyData.json'
+
 import rawMccData from './dataSources/mccData.json'
+import { initCurrencyDb } from './initCurrencyDb'
+
+const { currencyDb, currencyToCountryDb } = initCurrencyDb()
 
 export class DirectoryService {
-  static getCurrencyList() {
-    return currencyData
+  static getCurrencyList(ip: string) {
+    const country = lookup(ip)?.country,
+      currencyCodeForCountry = currencyToCountryDb.find(
+        ({ countryCode }) => countryCode == country,
+      )?.currencyCode,
+      currentIndexForCurrency = _.findIndex(
+        currencyDb,
+        ({ code }) => code == currencyCodeForCountry,
+      )
+
+    return currentIndexForCurrency == -1
+      ? currencyDb
+      : arrayMove(currencyDb, currentIndexForCurrency, 0)
   }
 
   static mccData = (rawMccData as MCCInput[]).reduce<{
